ca0c4b0547992600c923775e39dd2335905cb5bb
fu-qingyun <1366773912@qq.com>
2022-04-21 21:46:42 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//void sushu(int a, int b)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = a; i <= b; i++)
+//	{
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (i == j)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入所要求的素数的范围:\n");
+//	scanf("%d %d", &a, &b);
+//	sushu(a, b);
+//	return 0;
+//}
+
+//int runnian(int year)
+//{
+//	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//	{
+//		return 1;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	printf("请输入一个年份\n");
+//	scanf("%d", &year);
+//	int n = runnian(year);
+//	if (n)
+//	{
+//		printf("是闰年\n");
+//	}
+//	else
+//	{
+//		printf("不是闰年\n");
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	char arr1[] = "cbaabcd";
+	char arr2[] = "abcd";
+
+	return 0;
+}
